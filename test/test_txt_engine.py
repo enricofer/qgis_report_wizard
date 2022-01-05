acf986f70bfa367b0818143234027aa9457101b6
Enrico Ferreguti <enricofer@gmail.com>
2022-01-05 12:02:09 +0100

    restructure txt tests

--- test/test_txt_engine.py
+++ test/test_txt_engine.py
@@ -47,36 +47,32 @@ templates_path = os.path.join(
 class TextEngineTest(unittest.TestCase):
     """Test odt algorithm construction."""
 
-    def init__(self, *args, **kwargs):
-        super(TextEngineTest, self).__init__(*args, **kwargs)
+    def setUp(self):
         self.target_md = "/tmp/OUTPUT.md"
         self.target_zip = self.target_md+".zip"
         if os.path.exists(self.target):
             os.remove(self.target)
         if os.path.exists(self.target_zip):
             os.remove(self.target_zip)
-        self.project = QgsProject(PARENT)
-        CANVAS.setProject(self.project)
-        self.loadedProject = self.project.read(os.path.join(templates_path,"sample_prj.qgs"))
-        self.vector_driver = self.project.mapLayersByName("testdata")[0]
 
-    def __setup(self):
-        self.target_md = "/tmp/OUTPUT.md"
-        self.target_zip = self.target_md+".zip"
-        if os.path.exists(self.target):
-            os.remove(self.target)
-        if os.path.exists(self.target_zip):
-            os.remove(self.target_zip)
-        self.project = QgsProject(PARENT)
-        CANVAS.setProject(self.project)
-        self.loadedProject = self.project.read(os.path.join(templates_path,"sample_prj.qgs"))
-        self.vector_driver = self.project.mapLayersByName("testdata")[0]
+    def testLoadProjectTxtEngine(self):  # pylint: disable=too-many-locals,too-many-statements
+        """
+        Test engine setup
+        """
+        project = QgsProject(PARENT)
+        CANVAS.setProject(project)
+        loadedProject = self.project.read(os.path.join(templates_path,"sample_prj.qgs"))
+        self.assertTrue(loadedProject)
 
     def testSetupTxtEngine(self):  # pylint: disable=too-many-locals,too-many-statements
         """
         Test engine setup
         """
-        engine = hypertext_renderer(IFACE, self.vector_driver, 100)
+        project = QgsProject(PARENT)
+        CANVAS.setProject(project)
+        loadedProject = self.project.read(os.path.join(templates_path,"sample_prj.qgs"))
+        vector_driver = self.project.mapLayersByName("testdata")[0]
+        engine = hypertext_renderer(IFACE, vector_driver, 100)
         self.assertEqual(len(engine.environment), 4)
         self.assertEqual(len(engine.environment["features"]), 6)
 
@@ -84,7 +80,11 @@ class TextEngineTest(unittest.TestCase):
         """
         Test engine setup
         """
-        engine = hypertext_renderer(IFACE, self.vector_driver, 100)
+        project = QgsProject(PARENT)
+        CANVAS.setProject(project)
+        loadedProject = self.project.read(os.path.join(templates_path,"sample_prj.qgs"))
+        vector_driver = self.project.mapLayersByName("testdata")[0]
+        engine = hypertext_renderer(IFACE, vector_driver, 100)
         template = os.path.join(templates_path,"tab_project.md")
         result = engine.render(template,self.target,embed_images=False)
         self.assertEqual(result, self.target_zip)
